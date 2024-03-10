7840ac0180fd6f395f440b5be5e7a8e2cc33043d
NinoosMoshi <ninos1357@yahoo.com>
2024-03-09 20:34:49 -0600

    shop service

--- frontEnd/shop4u/src/app/components/shop/shop.component.ts
+++ frontEnd/shop4u/src/app/components/shop/shop.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopService } from 'src/app/services/shop.service';
-import { IProduct } from 'src/app/model/product';
+import { IBrand, ICategory, IProduct } from 'src/app/model/product';
 import { ProductItemComponent } from './product-item/product-item.component';
 
 @Component({
@@ -14,14 +14,47 @@ import { ProductItemComponent } from './product-item/product-item.component';
 export class ShopComponent implements OnInit{
 
   products:IProduct[] = [];
-  response:any;
+  categories:ICategory[] = [];
+  brands:IBrand[] = [];
 
   constructor(private shopService:ShopService){}
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getCategories();
+    this.getBrands();
+  }
+
+
+  getProducts(){
     this.shopService.getProducts().subscribe({
       next:response =>{
         this.products = response.dataList;
+      },
+      error:error =>{
+        console.log(error)
+      }
+    })
+  }
+
+  getCategories(){
+    this.shopService.getCategories().subscribe({
+      next:response =>{
+        console.log(response)
+        this.categories = [{categoryId:0, categoryName:'ALL'}, ...response];
+      },
+      error:error =>{
+        console.log(error)
+      }
+    })
+  }
+
+
+  getBrands(){
+    this.shopService.getBrands().subscribe({
+      next:response =>{
+        console.log(response)
+        this.brands = [{brandId:0, brandName:'ALL'}, ...response];
       },
       error:error =>{
         console.log(error)
--- frontEnd/shop4u/src/app/services/shop.service.ts
+++ frontEnd/shop4u/src/app/services/shop.service.ts
@@ -1,20 +1,64 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPagination } from '../model/pagination';
-import { IProduct } from '../model/product';
+import { IBrand, ICategory, IProduct } from '../model/product';
 import { environment } from 'src/environments/environment';
+import { ShopParams } from '../model/shopparams';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
+  shopParams  = new ShopParams();
+  pagination?: IPagination<IProduct[]>;
+
   constructor(private http:HttpClient) { }
 
+
   // http://localhost:8080/api/shop/products?pageSize=10
-  getProducts() {
-    // return this.http.get<IPagination<IProduct[]>>(environment.apiUrl +'shop/products?pageSize=10');
-    return this.http.get<IPagination<IProduct[]>>(environment.apiUrl +'shop/products');
+  getProducts():Observable<IPagination<IProduct[]>> {
+    let params = new HttpParams();
+    console.log("page size:");
+    console.log(this.shopParams.pageSize);
+    if(this.shopParams.brandId > 0) params = params.append('brandId', this.shopParams.brandId);
+    if(this.shopParams.categoryId > 0) params = params.append('categoryId', this.shopParams.categoryId);
+    params = params.append('sort', this.shopParams.sort);
+
+    params = params.append('pageIndex', this.shopParams.pageIndex);
+    params = params.append('pageSize', environment.pageSize);
+    if(this.shopParams.search) params = params.append('search',this.shopParams.search.trim());
+
+    return this.http.get<IPagination<IProduct[]>>(environment.apiUrl +'shop/products', {params}).pipe(
+
+      map(response => {
+         this.pagination= response;
+         console.log(response);
+          return response;
+
+      })
+
+    );
+  }
+
+  // http://localhost:8080/api/shop/categories
+  getCategories() {
+    return this.http.get<ICategory[]>(environment.apiUrl +'shop/categories');
+  }
+
+  // http://localhost:8080/api/shop/brands
+  getBrands() {
+    return this.http.get<IBrand[]>(environment.apiUrl +'shop/brands');
+  }
+
+
+  setShopParams(params: ShopParams) {
+    this.shopParams = params;
+  }
+
+  getShopParams() {
+    return this.shopParams;
   }
 
 
